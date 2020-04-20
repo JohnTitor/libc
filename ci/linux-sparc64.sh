c384f273f3614ee793e989f5a8a0484be0ba5a14
Yuki Okushi <huyuumi.dev@gmail.com>
2020-04-21 08:04:10 +0900

    wip

--- ci/linux-sparc64.sh
+++ ci/linux-sparc64.sh
@@ -8,6 +8,7 @@ cd /qemu
 curl --retry 5 -LO https://cdimage.debian.org/cdimage/ports/10.0/sparc64/iso-cd/debian-10.0-sparc64-NETINST-1.iso
 7z e debian-10.0-sparc64-NETINST-1.iso boot/initrd.gz
 7z e debian-10.0-sparc64-NETINST-1.iso boot/sparc64
+ls
 mv sparc64 kernel
 rm debian-10.0-sparc64-NETINST-1.iso
 
