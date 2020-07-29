7318388fa0e80d5ef97a05239a85a79a8ad0a578
Dark Kirb <darkkirb@gmail.com>
2020-07-29 14:48:13 +0100

    Add DevkitPPC support
    DevkitPPC does not support unix sockets natively, meaning that bindings
    to these functions was removed for powerpc targets with "nintendo" as
    vendor
    
    Suggested target json files:
    
    Nintendo Gamecube:
    ```
    {
      "arch": "powerpc",
      "data-layout": "E-m:e-p:32:32-i64:64-n32",
      "dynamic-linking": false,
      "env": "newlib",
      "executables": true,
      "has-elf-tls": false,
      "has-rpath": true,
      "linker-flavor": "gcc",
      "llvm-target": "powerpc-eabi",
      "max-atomic-width": 32,
      "os": "dolphin",
      "target-c-int-width": "32",
      "target-endian": "big",
      "target-family": "unix",
      "target-mcount": "_mcount",
      "target-pointer-width": "32",
      "vendor": "nintendo"
    }
    ```
    
    Nintendo Wii:
    ```
    {
      "arch": "powerpc",
      "data-layout": "E-m:e-p:32:32-i64:64-n32",
      "dynamic-linking": false,
      "env": "newlib",
      "executables": true,
      "has-elf-tls": false,
      "has-rpath": true,
      "linker-flavor": "gcc",
      "llvm-target": "powerpc-eabi",
      "max-atomic-width": 32,
      "os": "revolution",
      "target-c-int-width": "32",
      "target-endian": "big",
      "target-family": "unix",
      "target-mcount": "_mcount",
      "target-pointer-width": "32",
      "vendor": "nintendo"
    }
    ```

--- src/unix/newlib/mod.rs
+++ src/unix/newlib/mod.rs
@@ -33,11 +33,13 @@ s! {
         pub ai_protocol: ::c_int,
         pub ai_addrlen: socklen_t,
 
+        #[cfg(not(all(target_arch = "powerpc", target_vendor = "nintendo")))]
         #[cfg(target_arch = "xtensa")]
         pub ai_addr: *mut sockaddr,
 
         pub ai_canonname: *mut ::c_char,
 
+        #[cfg(not(all(target_arch = "powerpc", target_vendor = "nintendo")))]
         #[cfg(not(target_arch = "xtensa"))]
         pub ai_addr: *mut sockaddr,
 
@@ -598,6 +600,7 @@ extern "C" {
     pub fn rand() -> ::c_int;
     pub fn srand(seed: ::c_uint);
 
+    #[cfg(not(all(target_arch = "powerpc", target_vendor = "nintendo")))]
     pub fn bind(fd: ::c_int, addr: *const sockaddr, len: socklen_t)
         -> ::c_int;
     pub fn clock_settime(
@@ -614,6 +617,7 @@ extern "C" {
     ) -> ::c_int;
     pub fn closesocket(sockfd: ::c_int) -> ::c_int;
     pub fn ioctl(fd: ::c_int, request: ::c_ulong, ...) -> ::c_int;
+    #[cfg(not(all(target_arch = "powerpc", target_vendor = "nintendo")))]
     pub fn recvfrom(
         fd: ::c_int,
         buf: *mut ::c_void,
@@ -622,6 +626,7 @@ extern "C" {
         addr: *mut sockaddr,
         addr_len: *mut socklen_t,
     ) -> isize;
+    #[cfg(not(all(target_arch = "powerpc", target_vendor = "nintendo")))]
     pub fn getnameinfo(
         sa: *const sockaddr,
         salen: socklen_t,
@@ -700,6 +705,9 @@ cfg_if! {
     } else if #[cfg(target_arch = "xtensa")] {
         mod xtensa;
         pub use self::xtensa::*;
+    } else if #[cfg(target_arch = "powerpc")] {
+        mod powerpc;
+        pub use self::powerpc::*;
     } else {
         // Only tested on ARM so far. Other platforms might have different
         // definitions for types and constants.
